ce98fe7fdcf6bdefb586b5a6fa297e7e4ae24afb
toontran <sontungtran99@gmail.com>
2023-11-22 12:14:19 -0500

    output: export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
 
+// module.exports = nextConfig
+const nextConfig = {
+    trailingSlash: true,
+    output: 'export',
+    images: {
+        unoptimized: true
+    }
+}
 module.exports = nextConfig
